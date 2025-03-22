e1e81cd267d0380a266e72fa6d9e797b56d7a486
kishlay <kishlay.kumar@digantara.co.in>
2025-03-22 18:07:17 +0530

    Remove unused assets and update project metadata for SliceNMorph 3D Studio

--- src/App.js
+++ src/App.js
@@ -5,19 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CTRUH</h1>
       </header>
+      <body>
+        <h1>hgihg</h1>
+      </body>
     </div>
   );
 }
